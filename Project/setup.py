4c68fa1a8ada6910084dca6d36002c691fba9282
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-06-10 21:10:21 +0430

    done project

---
+++ Project/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Project',
+    version='1',
+    packages=[''],
+    url='https://github.com/FarshidNooshi/Computer-Networking-CE.AUT/tree/main/Project',
+    license='',
+    author='Farshid Nooshi',
+    author_email='farshidnooshi726@hotmail.com',
+    description='project setup'
+)
